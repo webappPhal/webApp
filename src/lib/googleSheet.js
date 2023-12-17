f91cd6e48d1180ccfb7aac396e378b55a0ce33c8
somnathraz <somnathkhadanga810@gmail.com>
2023-12-17 11:22:54 +0530

    json

--- src/lib/googleSheet.js
+++ src/lib/googleSheet.js
@@ -1,9 +1,8 @@
 import { google } from "googleapis";
-import "dotenv/config";
 
 export const authentication = async () => {
   const auth = new google.auth.GoogleAuth({
-    keyFile: JSON.parse(process.env.GOOGLE_SHEETS_KEY),
+    keyFile: "formdata.json",
     scopes: "https://www.googleapis.com/auth/spreadsheets",
   });
   const client = await auth.getClient();
